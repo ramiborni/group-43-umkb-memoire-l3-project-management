38b24ea856de2f024c63fc497a0a6281731609af
Borni Ahmed Rami <borniahmedrami@gmail.com>
2021-04-30 15:12:00 +0100

    [Remove] Remove MaterialUI and change it with Tailwind CSS

--- components/layouts/LoginLayout.tsx
+++ components/layouts/LoginLayout.tsx
@@ -2,10 +2,9 @@ import React, {ReactNode} from "react"
 import GeneralHead from "../public/heads/GeneralHead"
 import LoginStyles from "../../styles/components/LoginLayout.module.css"
 import {useTranslation} from "next-i18next"
-import {Button} from "@material-ui/core"
 import {GetStaticProps} from "next"
 import Link from 'next/link'
-import { useRouter } from "next/dist/client/router"
+import {useRouter} from "next/dist/client/router"
 
 const LoginLayout = ({child} : {
     child: ReactNode
@@ -14,18 +13,9 @@ const LoginLayout = ({child} : {
 
     const {t} = useTranslation('common')
     return (
-        <>
-            <GeneralHead title={
-                t('login')
-            }></GeneralHead>
-            <main>
-                <Link href='/' locale={router.locale === 'en' ? 'fr' : 'en'}>
-                    <Button> {
-                        t('change-locale')
-                    } </Button>
-                </Link>
-                {child} </main>
-        </>
+        <div>
+
+        </div>
     )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import { appWithTranslation } from 'next-i18next'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component  {...pageProps} />
 }
 
 export default appWithTranslation(MyApp)
