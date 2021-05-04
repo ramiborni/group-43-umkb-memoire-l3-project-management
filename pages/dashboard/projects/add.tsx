14a42e62e00e37563e39a0c0b7f48ac0081b6018
Borni Ahmed Rami <borniahmedrami@gmail.com>
2021-05-04 05:46:05 +0100

    [Fix] Remove wrong import

--- pages/dashboard/projects/add.tsx
+++ pages/dashboard/projects/add.tsx
@@ -1,11 +1,15 @@
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import React from "react";
 import DashboardLayout from "../../../components/layouts/DashboardLayout";
+import InfoCard from "../../../components/specified/dashboard/index/InfoCard";
 
 const add = () => {
     return (
         <DashboardLayout>
             <div className="p-10 flex flex-col">
+                <InfoCard>
+                    
+                </InfoCard>
 
             </div>
         </DashboardLayout>
