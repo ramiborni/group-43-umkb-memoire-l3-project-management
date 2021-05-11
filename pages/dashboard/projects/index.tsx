653c3e78d710c7d92e13935004671e6d17073ad2
Borni Ahmed Rami <borniahmedrami@gmail.com>
2021-05-11 10:41:48 +0100

    [Update] Add some overview & project cards to projects page

--- pages/dashboard/projects/index.tsx
+++ pages/dashboard/projects/index.tsx
@@ -1,12 +1,17 @@
 import {useTranslation} from "next-i18next";
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import Link from "next/link";
-import React from "react";
+import React, {useState} from "react";
 import {
     Button,
     Col,
+    Dropdown,
+    Grid,
+    Icon,
+    IconButton,
     Nav,
     Row,
+    SelectPicker,
     Tag
 } from "rsuite";
 import DashboardLayout from "../../../components/layouts/DashboardLayout";
@@ -17,16 +22,52 @@ import UtilStyle from '../../../styles/Utilities.module.css'
 
 const index = () => {
     const {t} = useTranslation('dashboard');
-    const inProgressProjects = [{
-            title: 'Gestion de projet 2.0',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur at maxime eligendi asperiores in facere ea, corrupti ad consectetur dolorum placeat. Quia, natus eligendi ipsum voluptates voluptatum labore aperiam exercitationem.',
-            fields: ['IT', 'Economie']
-        }];
-    const finishedProjects = [{
+    const projects = [
+        {
             title: 'Gestion de projet 1.0',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur at maxime eligendi asperiores in facere ea, corrupti ad consectetur dolorum placeat. Quia, natus eligendi ipsum voluptates voluptatum labore aperiam exercitationem.',
+            deadline : '12/06/2021',
             fields: ['Marketing']
-        }]
+        },
+        {
+            title: 'Gestion de projet 2.0',
+            deadline : '29/07/2021',
+            fields: ['IT', 'Economie']
+        }, 
+    ]
+    const porjectStateList = [
+        {
+            "label": "All",
+            "value": "all"
+        },
+        {
+            "label": "Active",
+            "value": "active"
+        },
+        {
+            "label": "Finished",
+            "value": "finished"
+        },
+        {
+            "label": "Paused",
+            "value": "paused"
+        }, {
+            "label": "Stopped",
+            "value": "stopped"
+        }
+    ]
+    const filterList = [
+        {
+            "label": "Sorted By Deadline",
+            "value": "deadline"
+        }, {
+            "label": "Sorted By Creation Date",
+            "value": "creation"
+        }
+    ]
+
+
+    const [activeTab, setActiveTab] = useState("projects")
+    const handleSelect = (activeKey) => setActiveTab(activeKey);
 
     return (
         <DashboardLayout>
@@ -38,9 +79,7 @@ const index = () => {
                     className={
                         UtilStyle['has-padding-25']
                 }>
-                    <OverViewCard num={
-                            inProgressProjects.length
-                        }
+                    <OverViewCard num={8}
                         title="Current Projects"
                         color="primary"/>
                 </Col>
@@ -50,9 +89,7 @@ const index = () => {
                     className={
                         UtilStyle['has-padding-25']
                 }>
-                    <OverViewCard num={
-                            finishedProjects.length
-                        }
+                    <OverViewCard num={4}
                         title="Completed Projects"
                         color="success"/>
                 </Col>
@@ -67,13 +104,103 @@ const index = () => {
                         color="info"/>
                 </Col>
             </Row>
-
-            <Nav>
-                <Nav.Item eventKey="home">
-                    Home
+            <br/>
+            <Nav activeKey={activeTab}
+                onSelect={handleSelect}
+                appearance="subtle">
+                <Nav.Item eventKey="projects">
+                    <h4>Projects</h4>
                 </Nav.Item>
-                <Nav.Item eventKey="news">News</Nav.Item>
+                <Nav.Item eventKey="clients">
+                    <h4>Clients</h4>
+
+                </Nav.Item>
             </Nav>
+            <br/>
+            <Grid fluid>
+
+            <Row>
+                
+                <Col xs={8}
+                    md={2}
+                    lg={2}>
+                    <SelectPicker data={porjectStateList}
+                        searchable={false}
+                        cleanable={false}
+                        defaultValue="all"/>
+                </Col>
+                <Col xs={8}
+                    md={14}
+                    lg={6}>
+                    <SelectPicker data={filterList}
+                        searchable={false}
+                        cleanable={false}
+                        defaultValue="deadline"/>
+                </Col>
+                <Col xs={24} md={8} lgOffset={12}
+                    lg={4}
+                    className={
+                        `${UtilStyle['justify-end']} ${UtilStyle['align-end']}} ${UtilStyle['has-padding-top-25-mobile']}`
+                }>
+                    <Button block appearance="primary">ADD PROJECT</Button>
+                </Col>
+            </Row>
+            </Grid>
+            <br/>
+            <Grid fluid>
+
+            <Row> {
+                projects.map((p, index) => <Col key={index}
+                    xs={24}
+                    md={12}
+                    lg={6}
+                    className={
+                        UtilStyle['has-padding-15']
+                }>
+                    <InfoCard>
+                        <Row>
+                            <Col xs={20}>
+                                {
+                                <h5>{p.title}</h5>
+                            }</Col>
+                            <Col xs={4}>
+                                <Dropdown style={
+                                        {marginTop: '-5.9px'}
+                                    }
+                                    renderTitle={
+                                        () => {
+                                            return <IconButton size="lg" appearance="subtle"
+                                                icon={
+                                                    <Icon
+                                                size="5x"
+                                                icon="ellipsis-v"></Icon>
+                                                }
+                                                circle/>;
+                                        }
+                                }>
+                                    <Dropdown.Item>
+                                        <Icon icon="detail"/>
+                                        Show More Details
+                                    </Dropdown.Item>
+                                    <Dropdown.Item>
+                                        <Icon icon="list-ul"/>
+                                        Show Tasks
+                                    </Dropdown.Item>
+                                    <Dropdown.Item>
+                                        <Icon icon="user"/>
+                                        Show Client
+                                    </Dropdown.Item>
+                                </Dropdown>
+                            </Col>
+                        </Row>
+                        <br/>
+                        <span className={UtilStyle.infoDarkText}> Deadline : <strong>{p.deadline}</strong></span>
+
+
+                    </InfoCard>
+                </Col>)
+            } </Row>
+            </Grid>
 
         </DashboardLayout>
     );
