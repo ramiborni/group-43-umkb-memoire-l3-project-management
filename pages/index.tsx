22096ff45e52144f6eed2d4bd9e9469b50a85938
Borni Ahmed Ramy <borniahmedrami@gmail.com>
2021-06-26 16:07:04 +0100

    [Update] Update login & update libraries

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,33 +2,27 @@ import {useTranslation} from 'next-i18next'
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
 import React, {Fragment, useContext, useEffect, useState} from 'react'
 import LoginLayout from '../components/layouts/LoginLayout'
-import OutlinedInputTextField from '../components/public/inputs/OutlinedInputTextField'
 import {useRouter} from 'next/router'
 import Link from 'next/link'
-import LoginContext from '../context/LoginContext'
-import Notiflix from "notiflix";
-import PrimaryButton from '../components/public/buttons/PrimaryButton'
-import UtilStyle from '../styles/Utilities.module.css'
-import Card from '../components/public/InfoCard'
 import GeneralHead from '../components/public/heads/GeneralHead'
 import {
     Button,
     CardContent,
-    CardHeader,
-    Collapse,
     Grid,
     IconButton,
     InputAdornment,
+    Snackbar,
     TextField,
     Typography
 } from '@material-ui/core'
 import InfoCard from '../components/public/InfoCard'
-import { SnackbarProvider } from 'notistack';
-import { AccountCircle } from '@material-ui/icons';
+import {SnackbarProvider} from 'notistack';
+import {AccountCircle} from '@material-ui/icons';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
-import { InferGetStaticPropsType } from "next";
+import {InferGetStaticPropsType} from "next";
 import CloseIcon from '@material-ui/icons/Close';
+import {Alert} from '@material-ui/lab'
 
 /**
  * CREATED By Borni Ahmed Rami ( borniahmedrami@gmail.com / github : @rikiraspoutine ) in 04/29/2021 
@@ -37,22 +31,19 @@ import CloseIcon from '@material-ui/icons/Close';
  * TODO: ADD login.tsx to redirect to index.tsx
  */
 
-export default function Home(props: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Home(props : InferGetStaticPropsType < typeof getStaticProps >) {
     const {t} = useTranslation('login')
     const router = useRouter()
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const notistackRef = React.createRef();
+
+    const [open, setOpen] = useState(false)
 
 
     const login = () => {
         if (!(username && password)) {
-            // @ts-ignore
-            notistackRef.current.enqueueSnackbar(t('empty-form'), {
-                variant: 'warning',
-                autoHideDuration: 3000,
-                snack,
-            });
+            setOpen(true);
+
         } else {
             router.push('/dashboard');
         }
@@ -60,17 +51,25 @@ export default function Home(props: InferGetStaticPropsType<typeof getStaticProp
     }
     const snack = key => (
         <Fragment>
-            <IconButton  onClick={() => { this.props.closeSnackbar(key) }}>
-                   <CloseIcon/>
+            <IconButton onClick={
+                () => {
+                    this.props.closeSnackbar(key)
+                }
+            }>
+                <CloseIcon/>
             </IconButton>
         </Fragment>
     );
+    const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setOpen(false);
+      };
 
     return (
-        <SnackbarProvider TransitionComponent={Collapse} maxSnack={3}>
-           <LoginContext.Provider value={
-            {username, password}
-        }>
+        <>
             <GeneralHead title={
                 t('login')
             }></GeneralHead>
@@ -81,13 +80,14 @@ export default function Home(props: InferGetStaticPropsType<typeof getStaticProp
                         xs={12}
                         md={6}>
                         <InfoCard>
-                            <CardContent className="space-y-8" >
+                            <CardContent className="space-y-8">
                                 <Grid container={true}
-                                    direction="row"  
+                                    direction="row"
                                     justify="center"
                                     alignItems="center">
                                     <Grid item
-                                        xs={10} className="space-y-5">
+                                        xs={10}
+                                        className="space-y-5">
                                         <Typography className="font-bold" variant="h4">
                                             {
                                             t('login')
@@ -102,50 +102,97 @@ export default function Home(props: InferGetStaticPropsType<typeof getStaticProp
                                     </Grid>
                                 </Grid>
                                 <form className="space-y-5">
-                                    <TextField placeholder={t('username')} onChange={(event) => setUsername(event.target.value)} name="username" variant="outlined" key="username" fullWidth required  InputProps={{
-          startAdornment: (
-            <InputAdornment className="mr-2" position="end">
-              <AccountCircleOutlinedIcon color="action" />
-            </InputAdornment>
-          ),
-        }}
+                                    <TextField placeholder={
+                                            t('username')
+                                        }
+                                        onChange={
+                                            (event) => setUsername(event.target.value)
+                                        }
+                                        name="username"
+                                        variant="outlined"
+                                        key="username"
+                                        fullWidth
+                                        required
+                                        InputProps={
+                                            {
+                                                startAdornment: (
+                                                    <InputAdornment className="mr-2" position="end">
+                                                        <AccountCircleOutlinedIcon color="action"/>
+                                                    </InputAdornment>
+                                                )
+                                            }
+                                        }
                                         label={
                                             t('username')
                                     }></TextField>
-                                    <TextField placeholder={t('password')} InputProps={{
-          startAdornment: (
-            <InputAdornment className="mr-2" position="end">
-              <LockOutlinedIcon color="action" />
-            </InputAdornment>
-          ),
-        }} onChange={(event) => setPassword(event.target.value)} name="password" variant="outlined" key="password" fullWidth required
+                                    <TextField placeholder={
+                                            t('password')
+                                        }
+                                        InputProps={
+                                            {
+                                                startAdornment: (
+                                                    <InputAdornment className="mr-2" position="end">
+                                                        <LockOutlinedIcon color="action"/>
+                                                    </InputAdornment>
+                                                )
+                                            }
+                                        }
+                                        onChange={
+                                            (event) => setPassword(event.target.value)
+                                        }
+                                        name="password"
+                                        variant="outlined"
+                                        key="password"
+                                        fullWidth
+                                        required
                                         label={
                                             t('password')
                                     }></TextField>
 
-                                    <Button style={{height:'40px'}} onClick={() => login()} disableElevation fullWidth variant="contained" color="primary">
-                                        {t('login')}
-                                    </Button>
+                                    <Button style={
+                                            {height: '40px'}
+                                        }
+                                        onClick={
+                                            () => login()
+                                        }
+                                        disableElevation
+                                        fullWidth
+                                        variant="contained"
+                                        color="primary">
+                                        {
+                                        t('login')
+                                    } </Button>
 
-                                    
-                                    
+
                                 </form>
                                 <div className="text-center">
-                                   <Link href="/reset">
-                                   <Button variant="text" color="primary">
-                                        {t('forgot_password')}
-                                    </Button>
-                                   </Link>
+                                    <Link href="/reset">
+                                        <Button variant="text" color="primary">
+                                            {
+                                            t('forgot_password')
+                                        } </Button>
+                                    </Link>
                                 </div>
 
-                               
+
                             </CardContent>
                         </InfoCard>
                     </Grid>
-                </Grid>
+                </Grid> 
+                <Snackbar open={open} onClose={handleClose}
+                    anchorOrigin={
+                        {
+                            vertical: 'bottom',
+                            horizontal: 'left'
+                        }
+                    }
+                    autoHideDuration={6000}>
+                    <Alert variant="filled" onClose={handleClose} severity="error">
+                        Both of username & password shouldn't be empty
+                    </Alert>
+                </Snackbar>
             </LoginLayout>
-        </LoginContext.Provider>
-        </SnackbarProvider>
+        </>
 
     )
 }
