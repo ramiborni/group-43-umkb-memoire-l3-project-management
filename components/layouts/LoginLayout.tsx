1cd6c1d56ae1ac64bd723ce50062aadb5aaefec3
Borni Ahmed Ramy <borniahmedrami@gmail.com>
2021-06-29 11:21:30 +0100

    [Fix] login children

--- components/layouts/LoginLayout.tsx
+++ components/layouts/LoginLayout.tsx
@@ -2,7 +2,7 @@ import React, {ReactNode} from "react"
 import LoginStyles from "../../styles/components/LoginLayout.module.css"
 import {useTranslation} from "next-i18next"
 import { Container } from "@material-ui/core"
-const LoginLayout = ({children}) => {
+const LoginLayout = ({children:React.ReactNode}) => {
 
     const {t} = useTranslation('common')
     return (
